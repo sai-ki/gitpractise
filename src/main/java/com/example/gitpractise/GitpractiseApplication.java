dd1d936a9d6a9b18484ddb403b713a69750d7114
sai-ki <saikirankannuri@cmail.carleton.ca>
2019-11-02 18:28:01 -0400

    first commit

---
+++ src/main/java/com/example/gitpractise/GitpractiseApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitpractise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitpractiseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitpractiseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gitpractise/GitpractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitpractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitpractiseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
